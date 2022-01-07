59f72c63d0f6392a6d128bb70c4a6d387698a1b6
Guy Madison <avonfarmguymadison@gmail.com>
2022-01-07 15:16:42 -0800

    Initial commit

---
+++ external/clone_external.sh
@@ -0,0 +1,9 @@
+git clone https://github.com/KhronosGroup/OpenGL-Registry.git
+git clone https://github.com/KhronosGroup/SPIRV-Cross.git
+git clone https://github.com/KhronosGroup/SPIRV-Headers.git
+git clone https://github.com/KhronosGroup/SPIRV-Tools.git
+cd SPIRV-Tools/external
+git clone https://github.com/KhronosGroup/SPIRV-Headers.git
+cd ../..
+git clone https://github.com/lxfontes/ezxml.git
+git clone https://github.com/KhronosGroup/glslang.git
