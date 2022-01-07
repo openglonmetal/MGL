d6e5e8e252f5b43235807557d103e57310f7da64
openglonmetal <96672642+openglonmetal@users.noreply.github.com>
2022-01-07 14:58:53 -0800

    Add files via upload

---
+++ external/clone_external.sh
@@ -0,0 +1,6 @@
+git clone https://github.com/KhronosGroup/OpenGL-Registry.git
+git clone https://github.com/KhronosGroup/SPIRV-Cross.git
+git clone https://github.com/KhronosGroup/SPIRV-Headers.git
+git clone https://github.com/KhronosGroup/SPIRV-Tools.git
+git clone https://github.com/lxfontes/ezxml.git
+git clone https://github.com/KhronosGroup/glslang.git
