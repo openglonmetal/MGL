b77945aebbb82935d150ccd79c7c877862803638
Guy Madison <avonfarmguymadison@gmail.com>
2022-01-07 16:16:14 -0800

    Added clone_external.sh

---
+++ external/clone_external.sh
@@ -0,0 +1,12 @@
+git clone https://github.com/KhronosGroup/OpenGL-Registry.git
+git clone https://github.com/KhronosGroup/SPIRV-Cross.git
+git clone https://github.com/KhronosGroup/SPIRV-Headers.git
+git clone https://github.com/KhronosGroup/SPIRV-Tools.git
+<<<<<<< HEAD
+cd SPIRV-Tools/external
+git clone https://github.com/KhronosGroup/SPIRV-Headers.git
+cd ../..
+=======
+>>>>>>> d6e5e8e252f5b43235807557d103e57310f7da64
+git clone https://github.com/lxfontes/ezxml.git
+git clone https://github.com/KhronosGroup/glslang.git
