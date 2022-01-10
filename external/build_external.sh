d2a15965e5e33e2999c4b1a6edd24f7386d422e8
Guy Madison <avonfarmguymadison@gmail.com>
2022-01-09 18:18:22 -0800

    Added missing glfw with changes

---
+++ external/build_external.sh
@@ -0,0 +1,24 @@
+cd SPIRV-Cross
+mkdir build
+cd build
+cmake ..
+make
+cd ../..
+cd SPIRV-Headers
+mkdir build
+cd build
+cmake ..
+make
+cd ../..
+cd SPIRV-Tools
+mkdir build
+cd build
+cmake ..
+make
+cd ../..
+cd glslang
+mkdir build
+cd build
+cmake ..
+make
+cd ../..
