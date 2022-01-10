8226feca0d1fdee3a313df42d5f5c074f2ab4ff7
Guy Madison <avonfarmguymadison@gmail.com>
2022-01-09 18:29:01 -0800

    Added build script for external stuff

---
+++ external/install_external.sh
@@ -0,0 +1,16 @@
+cd SPIRV-Cross
+cd build
+sudo make install
+cd ../..
+cd SPIRV-Headers
+cd build
+sudo make install
+cd ../..
+cd SPIRV-Tools
+cd build
+sudo make install
+cd ../..
+cd glslang
+cd build
+sudo make install
+cd ../..
