75bf8f27b08844a0cd555bcd81839e116d3cb6ed
Dimitre <dimitre@users.noreply.github.com>
2022-01-20 15:34:22 -0300

    Shallow clones
    so it is faster to install

--- external/clone_external.sh
+++ external/clone_external.sh
@@ -1,9 +1,9 @@
-git clone https://github.com/KhronosGroup/OpenGL-Registry.git
-git clone https://github.com/KhronosGroup/SPIRV-Cross.git
-git clone https://github.com/KhronosGroup/SPIRV-Headers.git
-git clone https://github.com/KhronosGroup/SPIRV-Tools.git
+git clone https://github.com/KhronosGroup/OpenGL-Registry.git --depth 1
+git clone https://github.com/KhronosGroup/SPIRV-Cross.git --depth 1
+git clone https://github.com/KhronosGroup/SPIRV-Headers.git --depth 1
+git clone https://github.com/KhronosGroup/SPIRV-Tools.git --depth 1
 cd SPIRV-Tools/external
-git clone https://github.com/KhronosGroup/SPIRV-Headers.git
+git clone https://github.com/KhronosGroup/SPIRV-Headers.git --depth 1
 cd ../..
-git clone https://github.com/lxfontes/ezxml.git
-git clone https://github.com/KhronosGroup/glslang.git
+git clone https://github.com/lxfontes/ezxml.git --depth 1
+git clone https://github.com/KhronosGroup/glslang.git --depth 1
