6c940ade38a45e6080126c07ca389411162c6fad
conversy <conversy@gmail.com>
2022-01-23 11:42:45 +0100

    request opengl 4.1 instead of 4.6

--- test_mgl_glfw/main.cpp
+++ test_mgl_glfw/main.cpp
@@ -2828,7 +2828,7 @@ int main(int argc, const char * argv[])
     //glfwWindowHint(GLFW_WIN32_KEYBOARD_MENU, GLFW_TRUE);
 
     glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 4);
-    glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 6);
+    glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 1);
     glfwWindowHint(GLFW_OPENGL_FORWARD_COMPAT, GL_TRUE);
     glfwWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE);
     glfwWindowHint(GLFW_COCOA_RETINA_FRAMEBUFFER, GL_TRUE);
