faf8142d8ad9c43e5657edf687fe169e6fa70eda
Guy Madison <avonfarmguymadison@gmail.com>
2022-01-28 10:38:45 -0800

    Updated bind call to make layer and drawable on bind, gets rid of red flash

---
+++ external/OpenSudivTEst/OpenSudivTEst/AppDelegate.h
@@ -0,0 +1,14 @@
+//
+//  AppDelegate.h
+//  OpenSudivTEst
+//
+//  Created by Michael Larson on 1/28/22.
+//
+
+#import <Cocoa/Cocoa.h>
+
+@interface AppDelegate : NSObject <NSApplicationDelegate>
+
+
+@end
+
